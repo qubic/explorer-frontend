ed04f946f07b077a826ae7b3dba9abbfe89d0f0e
konan-5 <ikedahiroshi517@gmail.com>
2024-03-19 08:31:10 +0000

    Add fuse template environment

---
+++ src/app/App.js
@@ -0,0 +1,63 @@
+import '@mock-api';
+import BrowserRouter from '@fuse/core/BrowserRouter';
+import FuseLayout from '@fuse/core/FuseLayout';
+import FuseTheme from '@fuse/core/FuseTheme';
+import { SnackbarProvider } from 'notistack';
+import { useSelector } from 'react-redux';
+import rtlPlugin from 'stylis-plugin-rtl';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { selectCurrentLanguageDirection } from 'app/store/i18nSlice';
+import { selectUser } from 'app/store/userSlice';
+import themeLayouts from 'app/theme-layouts/themeLayouts';
+import { selectMainTheme } from 'app/store/fuse/settingsSlice';
+import withAppProviders from './withAppProviders';
+
+// import axios from 'axios';
+/**
+ * Axios HTTP Request defaults
+ */
+// axios.defaults.baseURL = "";
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
+
+const emotionCacheOptions = {
+  rtl: {
+    key: 'muirtl',
+    stylisPlugins: [rtlPlugin],
+    insertionPoint: document.getElementById('emotion-insertion-point'),
+  },
+  ltr: {
+    key: 'muiltr',
+    stylisPlugins: [],
+    insertionPoint: document.getElementById('emotion-insertion-point'),
+  },
+};
+
+const App = () => {
+  const langDirection = useSelector(selectCurrentLanguageDirection);
+  const mainTheme = useSelector(selectMainTheme);
+
+  return (
+    <CacheProvider value={createCache(emotionCacheOptions[langDirection])}>
+      <FuseTheme theme={mainTheme} direction={langDirection}>
+        <BrowserRouter>
+          <SnackbarProvider
+            maxSnack={5}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            classes={{
+              containerRoot: 'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99',
+            }}
+          >
+            <FuseLayout layouts={themeLayouts} />
+          </SnackbarProvider>
+        </BrowserRouter>
+      </FuseTheme>
+    </CacheProvider>
+  );
+};
+
+export default withAppProviders(App)();
