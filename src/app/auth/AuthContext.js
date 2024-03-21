140d6198d4944c4e16e5237274940204b0857310
konan-5 <ikedahiroshi517@gmail.com>
2024-03-21 09:08:35 +0000

    Add tx section in blockpage

--- src/app/auth/AuthContext.js
+++ src/app/auth/AuthContext.js
@@ -13,26 +13,24 @@ function AuthProvider({ children }) {
   useEffect(() => {
 
     axios.defaults.baseURL = "http://localhost:7003";
-    axios.defaults.headers.common.Authorization =`Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImEzMzJiZTZmLTdkNGMtNDk4Ny05ZjBiLWJlODAzMGE5MmIwZCIsInN1YiI6Imd1ZXN0QHF1YmljLmxpIiwianRpIjoiM2MwNmM2ZDgtNzQ4My00ODRiLWI1NmMtOTBjMDZlMTUzODcxIiwiUHVibGljIjoiIiwibmJmIjoxNzEwOTY1ODU0LCJleHAiOjE3MTEwNTIyNTQsImlhdCI6MTcxMDk2NTg1NCwiaXNzIjoiaHR0cHM6Ly9xdWJpYy5saS8iLCJhdWQiOiJodHRwczovL3F1YmljLmxpLyJ9.tCvBqauAT1wM7nXPm_xPRDyq3N_3HbvVZMTX3SAFrJOCTcRYkKXP6ipLX5rsgGFQnhh9ci9l8z48Fu0rTmLIkA`
-    setWaitAuthCheck(true)
-    
-    // axios.post(`${jwtServiceConfig.login}`,
-    //   {
-    //     userName: "guest@qubic.li",
-    //     password: "guest13@Qubic.li",
-    //     twoFactorCode: "",
-    //   }
-    // ).then((response) => {
-    //   axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
-    // })
-    //   .then(() => {
-    //     console.log(axios.defaults.headers.common.Authorization)
-    //     setWaitAuthCheck(true)
-    //   }
-    //   )
-    //   .catch((error) => {
-    //     console.log(error)
-    //   })
+  
+    axios.post(`${jwtServiceConfig.login}`,
+      {
+        userName: "guest@qubic.li",
+        password: "guest13@Qubic.li",
+        twoFactorCode: "",
+      }
+    ).then((response) => {
+      axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+    })
+      .then(() => {
+        console.log(axios.defaults.headers.common.Authorization)
+        setWaitAuthCheck(true)
+      }
+      )
+      .catch((error) => {
+        console.log(error)
+      })
 
   }, []);
 
--- src/app/main/apps/explorer/block/BlockPage.js
+++ src/app/main/apps/explorer/block/BlockPage.js
@@ -34,7 +34,8 @@ function BlockPage() {
                     <div className="flex flex-col gap-8">
                         <div className="flex items-center gap-8">
                             <FuseSvgIcon
-                                className="w-24 text-gray-50"
+                                className=" text-gray-50"
+                                size={24}
                                 role="button"
                             >
                                 heroicons-solid:chevron-left
@@ -43,6 +44,13 @@ function BlockPage() {
                                 className="text-32 leading-40 font-500 font-space">
                                 {formatString(block?.tick)}
                             </Typography>
+                            <FuseSvgIcon
+                                className=" text-gray-50"
+                                size={24}
+                                role="button"
+                            >
+                                heroicons-solid:chevron-right
+                            </FuseSvgIcon>
                         </div>
                         <Typography
                             className="text-14 leading-20 font-space text-gray-50">
@@ -121,16 +129,64 @@ function BlockPage() {
                         block &&
                         block?.transactions?.map((item) => (
                             <div className="flex flex-col p-12 border-[1px] rounded-8 border-gray-70" key={item.id}>
-                                <div className="flex items-center gap-16">
+                                <div className="flex items-center gap-16 mb-14">
                                     <Typography
                                         className="flex gap-4 tex-16 leading-20 text-gray-50 font-space py-2 px-8 bg-gray-70 rounded-full items-center">
                                         TX
                                         <FuseSvgIcon className="text-20 w-20 h-20 text-success-40">heroicons-solid:check</FuseSvgIcon>
                                     </Typography>
                                     <Typography
-                                    className="text-16 leading-20 opacity-70 font-space">
+                                        className="text-16 leading-20 opacity-70 font-space">
                                         {item.id}
                                     </Typography>
+                                </div>
+                                <div className="flex flex-col pt-14 border-t-[1px] border-gray-70">
+                                    <div className="flex flex-col gap-12 pr-12">
+                                        <div className="flex flex-col gap-8">
+                                            <div className="flex justify-between items-center">
+                                                <Typography
+                                                    className="text-14 leading-18 font-space text-gray-50">
+                                                    Source
+                                                </Typography>
+                                                <Typography
+                                                    className="text-14 leading-18 font-space text-gray-50">
+                                                    Type
+                                                </Typography>
+                                            </div>
+                                            <div className="flex justify-between items-center">
+                                                <Typography
+                                                    className="text-14 leading-18 font-space text-primary-40">
+                                                    {item.sourceId}
+                                                </Typography>
+                                                <Typography
+                                                    className="text-14 leading-18 font-space">
+                                                    0 Standard
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col gap-8">
+                                            <div className="flex justify-between items-center">
+                                                <Typography
+                                                    className="text-14 leading-18 font-space text-gray-50">
+                                                    Destination
+                                                </Typography>
+                                                <Typography
+                                                    className="text-14 leading-18 font-space text-gray-50">
+                                                    Amount
+                                                </Typography>
+                                            </div>
+                                            <div className="flex justify-between items-center">
+                                                <Typography
+                                                    className="text-14 leading-18 font-space text-primary-40">
+                                                    {item.destId}
+                                                </Typography>
+                                                <Typography
+                                                    className="text-14 leading-18 font-space">
+                                                    0 qus
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         ))
