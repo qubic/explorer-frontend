4b8b18386eeccb8262fa7b1cf93930f83b67caf1
konan-5 <ikedahiroshi517@gmail.com>
2024-03-22 16:12:10 +0000

    Update address page responsive

--- src/app/main/apps/network/address/AddressPage.js
+++ src/app/main/apps/network/address/AddressPage.js
@@ -5,6 +5,8 @@ import { Typography } from "@mui/material";
 import AddressLink from "../component/AddressLink";
 import TxLink from "../component/TxLink";
 import TxStatus from "../component/TxStatus";
+import CardItem from "../component/CardItem";
+import TickLink from "../component/TickLink";
 
 import { getAddress, selectAddress } from "../store/addressSlice";
 
@@ -21,23 +23,64 @@ function AddressPage() {
 
     const address = useSelector(selectAddress)
 
+    console.log(address)
+
     return (
         <div className="w-full">
-            <div className="pt-82 pb-40 max-w-[960px] mx-auto px-8">
+            <div className="pt-82 pb-40 max-w-[960px] mx-auto px-12">
                 <Typography
-                    className="font-space text-16 leading-20 mb-12">
+                    className="font-space text-16 leading-20 mb-12 text-gray-50">
                     Tick
                 </Typography>
                 <Typography
-                    className="font-space text-24 leading-30 mb-32">
+                    className="font-space text-24 leading-30 mb-32 break-all">
                     {address?.id}
                 </Typography>
                 <Typography
                     className="font-space text-14 leading-18 mb-12">
                     Entity Reports from random peers
                 </Typography>
-                <div className="mb-32">
-                    node
+                <div className="grid 948px:grid-cols-3 gap-16 mb-32">
+                    {
+                        address &&
+                        Object.entries(address?.reportedValues).map(([ip, details]) => (
+                            <CardItem
+                                className="p-16" key={ip}>
+                                <div className="flex justify-between mb-16">
+                                    <div className="">
+                                        <Typography
+                                            className="text-14 leading-18 font-space text-gray-50">
+                                            Value
+                                        </Typography>
+                                        <Typography
+                                            className="text-16 leading-20 font-space font-500">
+                                            1 QUBIC
+                                        </Typography>
+                                    </div>
+                                    <Typography
+                                        className="text-14 leading-18 font-space text-gray-50">
+                                        {ip}
+                                    </Typography>
+                                </div>
+                                <div className="flex flex-col items-center 948px:items-start gap-8">
+                                    <Typography
+                                        className="text-14 leading-18 font-space text-gray-50">
+                                        Incoming:
+                                        <span className="text-white"> {details.numberOfIncomingTransfers} </span>(Latest: <TickLink
+                                            value={details.latestIncomingTransferTick}
+                                            className="text-primary-40 break-all" />)
+                                    </Typography>
+                                    <Typography
+                                        className="text-14 leading-18 font-space text-gray-50">
+                                        Outgoing:
+                                        <span className="text-white"> {details.numberOfOutgoingTransfers} </span>(Latest: <TickLink
+                                            value={details.latestOutgoingTransferTick}
+                                            className="text-primary-40 break-all" />)
+                                    </Typography>
+                                </div>
+                            </CardItem>
+                        ))
+                    }
                 </div>
                 <Typography
                     className="text-20 leading-26 font-500 font-space mb-16">
@@ -47,52 +90,53 @@ function AddressPage() {
                     {
                         address &&
                         address?.latestTransfers?.map((item) => (
-
                             <div className="flex flex-col p-12 border-[1px] rounded-8 border-gray-70" key={item.id}>
-                                <div className="flex items-center gap-16 mb-14">
-                                    <TxStatus
-                                        executed={item.executed} />
+                                <div className="flex flex-col md:flex-row md:items-center gap-10 md:gap-16 mb-14">
+                                    <div className="">
+                                        <TxStatus
+                                            executed={item.executed} />
+                                    </div>
                                     <TxLink
                                         value={item.id} />
                                 </div>
                                 <div className="flex flex-col pt-14 border-t-[1px] border-gray-70">
-                                    <div className="flex flex-col gap-12 pr-12">
-                                        <div className="flex flex-col gap-8">
-                                            <div className="flex justify-between items-center">
+                                    <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-16">
+                                        <div className="flex flex-col gap-16">
+                                            <div className="flex flex-col gap-8">
                                                 <Typography
                                                     className="text-14 leading-18 font-space text-gray-50">
                                                     Source
                                                 </Typography>
+                                                <AddressLink
+                                                    value={item.sourceId}
+                                                />
+                                            </div>
+                                            <div className="flex flex-col gap-8">
+                                                <Typography
+                                                    className="text-14 leading-18 font-space text-gray-50">
+                                                    Destination
+                                                </Typography>
+                                                <AddressLink
+                                                    value={item.destId}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col sm:flex-row md:flex-col gap-24 pr-24">
+                                            <div className="flex flex-col gap-5 md:items-end">
                                                 <Typography
                                                     className="text-14 leading-18 font-space text-gray-50">
                                                     Type
                                                 </Typography>
-                                            </div>
-                                            <div className="flex justify-between items-center">
-                                                <AddressLink
-                                                    value={item.sourceId}
-                                                />
                                                 <Typography
                                                     className="text-14 leading-18 font-space">
                                                     {item.type} Standard
                                                 </Typography>
                                             </div>
-                                        </div>
-                                        <div className="flex flex-col gap-8">
-                                            <div className="flex justify-between items-center">
-                                                <Typography
-                                                    className="text-14 leading-18 font-space text-gray-50">
-                                                    Destination
-                                                </Typography>
+                                            <div className="flex flex-col gap-5 md:items-end">
                                                 <Typography
                                                     className="text-14 leading-18 font-space text-gray-50">
                                                     Amount
                                                 </Typography>
-                                            </div>
-                                            <div className="flex justify-between items-center">
-                                                <AddressLink
-                                                    value={item.destId}
-                                                />
                                                 <Typography
                                                     className="text-14 leading-18 font-space">
                                                     {item.amount} qus
