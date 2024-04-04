0b85e6e558c2d8e58ab88dc57ad4f8aadfae8831
konan-5 <ikedahiroshi517@gmail.com>
2024-04-04 18:08:52 +0000

    Implement autofocus in global search

--- src/app/main/apps/network/NetworkHeader.js
+++ src/app/main/apps/network/NetworkHeader.js
@@ -4,17 +4,17 @@ import SearchBar from 'src/app/components/SearchBar/SearchBar';
 
 function NetworkHeader() {
   return (
-    <div className="p-20 flex mx-4 gap-6 justify-center items-center">
+    <div className="p-20 relative flex max-w-[1440px] mx-auto gap-6 justify-center items-center">
       <Link to="/network">
         <img src="assets/images/logo/logo-text-on-dark.svg" alt="logo" />
       </Link>
-      <div className="absolute right-24">
+      <div className="absolute right-12 sm:right-24">
         <div className="hidden sm:inline">
           <SearchBar />
         </div>
         <LanguagePicker />
       </div>
-      <div className="absolute left-24 sm:hidden">
+      <div className="absolute left-12 sm:left-24 sm:hidden">
         <SearchBar />
       </div>
     </div>
