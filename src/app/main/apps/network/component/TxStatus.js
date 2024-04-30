c8c786f410835b2b42d706abe3ddfdb8cab4fd13
konan-5 <ikedahiroshi517@gmail.com>
2024-04-30 18:42:02 +0000

    Remove hover status in tx item

--- src/app/main/apps/network/component/TxStatus.js
+++ src/app/main/apps/network/component/TxStatus.js
@@ -1,18 +1,18 @@
-import { IconButton, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
 
 function TxStatus(props) {
   const { executed } = props;
 
   return (
-    <IconButton className="flex gap-4 py-2 px-8 bg-gray-70 rounded-full items-center">
+    <div className="flex gap-4 py-2 px-8 bg-gray-70 rounded-full items-center">
       <Typography className=" tex-16 leading-20 text-gray-50 font-space ">TX</Typography>
       <FuseSvgIcon
         className={`text-20 w-20 h-20 ${executed ? 'text-success-40' : 'text-error-40'}`}
       >
         {executed ? 'heroicons-solid:check' : 'heroicons-solid:x'}
       </FuseSvgIcon>
-    </IconButton>
+    </div>
   );
 }
 
