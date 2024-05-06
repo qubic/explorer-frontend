59cdf4e758a93d277cb0082cb41f68e5534fc494
AndyQus <AndyQus@outlook.com>
2024-05-06 12:05:35 +0200

    china bug and version 1.3

--- src/i18n.js
+++ src/i18n.js
@@ -11,28 +11,29 @@ const resources = {
   },
 };
 
-const getInitialLanguage = () => {
-  // Check for a stored language in localStorage
-  const storedLang = localStorage.getItem('lng');
-  if (storedLang) {
-    return storedLang;
-  }
+// const getInitialLanguage = () => {
+//   // Check for a stored language in localStorage
+//   const storedLang = localStorage.getItem('lng');
+//   if (storedLang) {
+//     return storedLang;
+//   }
 
-  // Get browser language
-  const browserLang = navigator.language.split('-')[0]; // Extract base language, e.g., 'en' from 'en-US'
-  if (browserLang) {
-    return browserLang;
-  }
+//   // Get browser language
+//   const browserLang = navigator.language.split('-')[0]; // Extract base language, e.g., 'en' from 'en-US'
+//   if (browserLang) {
+//     return browserLang;
+//   }
 
-  // Default to English if the browser language is not supported
-  return 'en';
-};
+//   // Default to English if the browser language is not supported
+//   return 'en';
+// };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: getInitialLanguage(),
+    lng: localStorage.getItem('lng') || 'en',
+    // lng: getInitialLanguage(),
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
