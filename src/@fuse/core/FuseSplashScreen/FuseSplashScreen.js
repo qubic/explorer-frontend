dc777a3998b1f5e3f80114d79650f04d3351aaaf
alexmf91 <alexmunoz91@gmail.com>
2024-06-26 22:35:44 +0200

    refactor: Replace FuseSplashScreen with AppLoader component for loading animation

--- src/@fuse/core/FuseSplashScreen/FuseSplashScreen.js
+++ src/@fuse/core/FuseSplashScreen/FuseSplashScreen.js
@@ -1,26 +1,8 @@
 import { memo } from 'react';
-import Box from '@mui/material/Box';
+import AppLoader from 'src/app/components/AppLoader';
 
 function FuseSplashScreen() {
-  return (
-    <div id="fuse-splash-screen">
-      <div className="logo">
-        <img width="128" src="assets/images/logo/logo.svg" alt="logo" />
-      </div>
-      <Box
-        id="spinner"
-        sx={{
-          '& > div': {
-            backgroundColor: 'palette.secondary.main',
-          },
-        }}
-      >
-        <div className="bounce1" />
-        <div className="bounce2" />
-        <div className="bounce3" />
-      </Box>
-    </div>
-  );
+  return <AppLoader />;
 }
 
 export default memo(FuseSplashScreen);
