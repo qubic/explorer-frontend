4948d90e22f7bcdc8e6ce2ff61dbd8b5b2dab225
alexmf91 <alexmunoz91@gmail.com>
2024-06-27 22:52:39 +0200

    refactor: Remove unused footer

--- src/app/main/apps/network/overview/Overview.js
+++ src/app/main/apps/network/overview/Overview.js
@@ -1,11 +1,4 @@
-import {
-  LinearProgress,
-  Link,
-  Pagination,
-  PaginationItem,
-  Tooltip,
-  Typography,
-} from '@mui/material';
+import { LinearProgress, Pagination, PaginationItem, Tooltip, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useEffect, useState } from 'react';
@@ -256,43 +249,6 @@ function Overview() {
             />
           </div>
         </CardItem>
-      </div>
-      <div className="container px-12 py-40 flex flex-col sm:flex-row justify-center items-center gap-10 sm:gap-32 ">
-        <div className="flex items-center gap-10">
-          <img className="h-16" src="assets/images/logo/logo-text-short.svg" alt="logo-short" />
-          <Typography className="text-12 font-space text-gray-50">
-            {'\u00A9'} {new Date().getFullYear()} Qubic.
-          </Typography>
-        </div>
-        <div className="flex items-center gap-10">
-          <Link
-            href="https://qubic.org/Terms-of-service"
-            className="text-12 font-space text-white"
-            target="_blank"
-            role="button"
-          >
-            Terms of service
-          </Link>
-          <span className="text-gray-50">•</span>
-          <Link
-            href="https://qubic.org/Privacy-policy"
-            className="text-12 font-space text-white"
-            target="_blank"
-            role="button"
-          >
-            Privacy Policy
-          </Link>
-          <span className="text-gray-50">•</span>
-          <Link
-            href="https://status.qubic.li"
-            className="text-12 font-space text-white"
-            target="_blank"
-            role="button"
-          >
-            Network Status
-          </Link>
-        </div>
-        <Typography className="text-12 font-space text-gray-50">Version 1.4.1</Typography>
       </div>
     </div>
   );
