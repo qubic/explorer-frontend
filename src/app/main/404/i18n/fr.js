77e68f19a8d4197a933b0951d4bf39a2e7d8664f
alexmf91 <alexmunoz91@gmail.com>
2024-07-22 20:52:47 +0200

    chore: Remove code related to template and js files to migrate

--- src/app/main/404/i18n/fr.js
+++
@@ -1,7 +0,0 @@
-const locale = {
-  pageNotFound: 'Page non trouvée',
-  backToHomePage: "Retour à la page d'accueil",
-  error404Message:
-    "Félicitations ! Vous faites partie de l'élite qui a réussi à trouver notre page 404.",
-};
-export default locale;
