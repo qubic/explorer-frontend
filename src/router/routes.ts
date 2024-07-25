7d84e71bd21b0d59df692165545921e9eb34ee75
alexmf91 <alexmunoz91@gmail.com>
2024-07-25 18:35:53 +0200

    feat: Add address, transaction, and tick routes to network routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,9 @@
 const Routes = {
   NETWORK: {
-    ROOT: '/network'
+    ROOT: '/network',
+    ADDRESS: (address: string) => `${Routes.NETWORK.ROOT}/address/${address}`,
+    TX: (txId: string) => `${Routes.NETWORK.ROOT}/tx/${txId}`,
+    TICK: (tick: string | number) => `${Routes.NETWORK.ROOT}/tick/${tick}`
   },
   NOT_FOUND: '*'
 } as const
