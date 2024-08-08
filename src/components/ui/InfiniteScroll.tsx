b60af74b4df94efe6963f3ce85fe3e706cd4900c
alexmf91 <alexmunoz91@gmail.com>
2024-08-08 21:13:45 +0200

    feat: Add custom InfiniteScroll component with IntersectionObserver impl

---
+++ src/components/ui/InfiniteScroll.tsx
@@ -0,0 +1,74 @@
+import { clsxTwMerge } from '@app/utils'
+import type React from 'react'
+import { useCallback, useRef, useState } from 'react'
+import { DotsLoader } from './loaders'
+
+interface InfiniteScrollProps<T> {
+  items: T[] // Array of items to display
+  loadMore: () => void | Promise<void> // Function to load more items
+  hasMore: boolean // Boolean indicating if more items can be loaded
+  renderItem: (item: T, index: number) => React.ReactNode // Render function for each item
+  loader?: React.ReactNode // Custom loader component
+  endMessage?: React.ReactNode // Message to display when all items are loaded
+  threshold?: number // Threshold to trigger load more function
+  className?: string // Custom class name for list container
+}
+
+export default function InfiniteScroll<T>({
+  items,
+  loadMore,
+  hasMore,
+  renderItem,
+  loader = <DotsLoader />,
+  endMessage,
+  threshold = 0,
+  className
+}: InfiniteScrollProps<T>) {
+  const observer = useRef<IntersectionObserver | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleLoadMore = useCallback(() => {
+    const result = loadMore()
+    setIsLoading(true)
+    if (result instanceof Promise) {
+      result.finally(() => setIsLoading(false))
+    } else {
+      setTimeout(() => setIsLoading(false), 1500)
+    }
+  }, [loadMore])
+
+  const lastElementRef = useCallback(
+    (node: HTMLElement | null) => {
+      if (observer.current) observer.current.disconnect()
+
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting && hasMore && !isLoading) {
+            handleLoadMore()
+          }
+        },
+        { rootMargin: `${threshold}px` }
+      )
+
+      if (node) observer.current.observe(node)
+    },
+    [handleLoadMore, hasMore, isLoading, threshold]
+  )
+
+  return (
+    <div>
+      <ul className={clsxTwMerge('space-y-12', className)}>
+        {items.map((item, index) => (
+          <li
+            key={JSON.stringify(item)}
+            ref={index === items.length - 1 ? lastElementRef : undefined}
+          >
+            {renderItem(item, index)}
+          </li>
+        ))}
+      </ul>
+      {isLoading && loader}
+      {!hasMore && !isLoading && endMessage}
+    </div>
+  )
+}
