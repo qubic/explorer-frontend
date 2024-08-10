7e185ecd99cd813a8c3d39b20f0eb21e4c42e94c
alexmf91 <alexmunoz91@gmail.com>
2024-08-10 10:04:59 +0200

    refactor: Add draft txSlice

--- src/store/network/networkReducer.ts
+++ src/store/network/networkReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import blockReducer from './blockSlice'
 import overviewReducer from './overviewSlice'
+import txReducer from './txSlice'
 
 export const networkReducer = combineReducers({
   overview: overviewReducer,
-  block: blockReducer
+  block: blockReducer,
+  tx: txReducer
 })
 
 export type NetworkState = ReturnType<typeof networkReducer>
---
+++ src/store/network/txSlice.ts
@@ -0,0 +1,87 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import type { Transaction, TransactionStatus } from '@app/services/archiver'
+import { archiverApiService } from '@app/services/archiver'
+import { qliApiService } from '@app/services/qli'
+import type { RootState } from '@app/store'
+import type { TxType } from '@app/types'
+import { convertHistoricalTxToTxWithStatus } from './adapters'
+
+export type TransactionWithStatus = {
+  tx: Transaction
+  status: TransactionStatus
+}
+
+export interface TxState {
+  txWithStatus: TransactionWithStatus | null
+  isLoading: boolean
+  error: string | null
+}
+
+const initialState: TxState = {
+  txWithStatus: null,
+  isLoading: false,
+  error: null
+}
+
+type GetTxArgs = {
+  txId: string | undefined
+  txType: TxType
+}
+
+export const getTx = createAsyncThunk<TransactionWithStatus, GetTxArgs>(
+  'network/tx',
+  async ({ txId, txType }) => {
+    if (!txId) {
+      throw new Error('Invalid transaction ID')
+    }
+
+    if (txType === 'historical') {
+      const historicalTx = null
+      // TODO: Implement getting historical transaction data from state once address slice it's implemented
+      // const historicalTx = getState().network.address.historicalTxs.data.find(
+      //   (tx) => tx.id === txId
+      // )
+
+      if (!historicalTx) {
+        const getHistoricalTxResult = await qliApiService.getTransaction(txId)
+        return convertHistoricalTxToTxWithStatus(getHistoricalTxResult)
+      }
+      // TODO: Implement getting historical transaction data from state once address slice it's implemented
+      // return convertHistoricalTxToTxWithStatus(historicalTx)
+    }
+
+    const [{ transaction }, { transactionStatus }] = await Promise.all([
+      archiverApiService.getTransaction(txId),
+      archiverApiService.getTransactionStatus(txId)
+    ])
+
+    return { tx: transaction, status: transactionStatus }
+  }
+)
+
+const txSlice = createSlice({
+  name: 'network/tx',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTx.pending, (state) => {
+        state.isLoading = true
+        state.error = null
+      })
+      .addCase(getTx.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.txWithStatus = action.payload
+      })
+      .addCase(getTx.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.error.message || 'Something went wrong'
+      })
+  }
+})
+
+// Selectors
+export const selectTx = (state: RootState) => state.network.tx
+
+export default txSlice.reducer
