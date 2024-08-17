0b13c60acd2647e659ca98a26545acb188da945d
alexmf91 <alexmunoz91@gmail.com>
2024-08-17 11:32:59 +0200

    feat: Add AddressPage and export it in network index.ts

--- src/pages/network/index.ts
+++ src/pages/network/index.ts
@@ -1,3 +1,4 @@
+export { default as AddressPage } from './address/AddressPage'
 export { default as OverviewPage } from './OverviewPage'
 export { default as TickPage } from './TickPage'
 export { default as TxPage } from './TxPage'
