1eede24d46155f81bc0bf42c686415fc3543f97e
alexmf91 <alexmunoz91@gmail.com>
2024-08-17 11:26:11 +0200

    feat: Add ChevronToggleButton component

---
+++ src/components/ui/buttons/ChevronToggleButton.tsx
@@ -0,0 +1,35 @@
+import { ChevronDownIcon } from '@app/assets/icons'
+import { clsxTwMerge } from '@app/utils'
+
+type Props = {
+  isOpen?: boolean
+} & React.HTMLAttributes<HTMLButtonElement>
+
+export default function ChevronToggleButton({
+  onClick,
+  className,
+  children,
+  isOpen = false,
+  ...rest
+}: Props) {
+  return (
+    <button
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...rest}
+      type="button"
+      className={clsxTwMerge(
+        'flex size-fit items-center gap-12 rounded-8 px-6 py-3 hover:bg-primary-30',
+        className
+      )}
+      onClick={onClick}
+    >
+      {children && <span className="text-center font-space text-sm">{children}</span>}
+      <ChevronDownIcon
+        className={clsxTwMerge(
+          'h-20 w-20 text-gray-50 transition-transform duration-300',
+          isOpen ? 'rotate-180' : 'rotate-0'
+        )}
+      />
+    </button>
+  )
+}
