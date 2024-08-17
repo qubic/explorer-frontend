c553663b001dd23a76e6ceae8ee6029246e235e2
alexmf91 <alexmunoz91@gmail.com>
2024-08-17 11:30:41 +0200

    refactor: Update ErrorMessage component to use consistent CSS class names and improve readability

--- src/pages/network/components/ErrorMessage.tsx
+++ src/pages/network/components/ErrorMessage.tsx
@@ -1,14 +1,11 @@
-import { Typography } from '@app/components/ui'
 import { useTranslation } from 'react-i18next'
 
 function ErrorMessage() {
-  const { t } = useTranslation('networkPage')
+  const { t } = useTranslation('network-page')
 
   return (
-    <div className="w-full py-12 bg-warning-90">
-      <Typography className="w-full text-15 leading-36 text-warning-40 text-center">
-        {t('errorMessage')}
-      </Typography>
+    <div className="w-full bg-warning-90 py-12">
+      <p className="w-full text-center text-15 leading-36 text-warning-40">{t('errorMessage')}</p>
     </div>
   )
 }
