00d26e4d798f459fafa4a7786d379f3ef0e0759a
alexmf91 <alexmunoz91@gmail.com>
2024-08-18 13:39:03 +0200

    refactor: Remove unused exports

--- src/pages/network/index.ts
+++ src/pages/network/index.ts
@@ -1,9 +1,6 @@
 import { lazy } from 'react'
 
-export { default as AddressPage } from './address/AddressPage'
 export { default as OverviewPage } from './OverviewPage'
-export { default as TickPage } from './TickPage'
-export { default as TxPage } from './TxPage'
 
 export const AddressPageLazy = lazy(() => import('./address/AddressPage'))
 export const TickPageLazy = lazy(() => import('./TickPage'))
