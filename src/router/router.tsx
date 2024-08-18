83be75d96566876d5cde3a4b9e681c23333c79c0
alexmf91 <alexmunoz91@gmail.com>
2024-08-18 10:49:01 +0200

    refactor: Update router to handle dynamic paths for tick, tx, and address pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,8 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+
 import { AppLayout } from '@app/components/ui/layouts'
 import { Error404Page } from '@app/pages'
 import { AddressPage, OverviewPage, TickPage, TxPage } from '@app/pages/network'
-import { createBrowserRouter, Navigate } from 'react-router-dom'
 import Routes from './routes'
 
 const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
@@ -22,31 +23,16 @@ const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
             element: <OverviewPage />
           },
           {
-            path: 'tick',
-            children: [
-              {
-                path: ':tick',
-                element: <TickPage />
-              }
-            ]
+            path: 'tick/:tick',
+            element: <TickPage />
           },
           {
-            path: 'tx',
-            children: [
-              {
-                path: ':txId',
-                element: <TxPage />
-              }
-            ]
+            path: 'tx/:txId',
+            element: <TxPage />
           },
           {
-            path: 'address',
-            children: [
-              {
-                path: ':addressId',
-                element: <AddressPage />
-              }
-            ]
+            path: 'address/:addressId',
+            element: <AddressPage />
           }
         ]
       }
