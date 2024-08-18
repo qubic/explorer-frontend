f4e0a20a8604efae67f2d18ab06daef589ebd287
alexmf91 <alexmunoz91@gmail.com>
2024-08-19 00:42:40 +0200

    refactor: Update app layout component adding error boundary

--- src/components/ui/layouts/AppLayout.tsx
+++ src/components/ui/layouts/AppLayout.tsx
@@ -1,25 +1,23 @@
 import { Suspense } from 'react'
-import { Outlet, useNavigation } from 'react-router-dom'
-import { AppLoader } from '../loaders'
+import { Outlet } from 'react-router-dom'
+
+import { ErrorBoundary } from '@app/components/ui/error-boundaries'
+import { AppLoader, LinearProgress } from '@app/components/ui/loaders'
 import Footer from './Footer'
 import Header from './Header'
 
 export default function AppLayout() {
-  const navigation = useNavigation()
-
-  if (navigation.state === 'loading') {
-    return <AppLoader />
-  }
-
   return (
-    <>
+    <Suspense fallback={<AppLoader />}>
       <Header />
       <main className="min-h-[var(--container-height)] w-full sm:min-h-[var(--desktop-container-height)]">
-        <Suspense fallback={<AppLoader />}>
-          <Outlet />
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<LinearProgress />}>
+            <Outlet />
+          </Suspense>
+        </ErrorBoundary>
       </main>
       <Footer />
-    </>
+    </Suspense>
   )
 }
