ac434fe938050aa526b71e5dba873a3679d1834e
alexmf91 <alexmunoz91@gmail.com>
2024-08-18 13:37:34 +0200

    refactor: Lazily load AddressPage, TickPage, and TxPage components

--- src/pages/network/index.ts
+++ src/pages/network/index.ts
@@ -1,4 +1,10 @@
+import { lazy } from 'react'
+
 export { default as AddressPage } from './address/AddressPage'
 export { default as OverviewPage } from './OverviewPage'
 export { default as TickPage } from './TickPage'
 export { default as TxPage } from './TxPage'
+
+export const AddressPageLazy = lazy(() => import('./address/AddressPage'))
+export const TickPageLazy = lazy(() => import('./TickPage'))
+export const TxPageLazy = lazy(() => import('./TxPage'))
