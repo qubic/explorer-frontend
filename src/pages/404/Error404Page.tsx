0c61ef1af9279867794b9114f5f79d21e28559cb
alexmf91 <alexmunoz91@gmail.com>
2024-08-19 21:52:35 +0200

    refactor: Update Error404Page component and error handling

--- src/pages/404/Error404Page.tsx
+++ src/pages/404/Error404Page.tsx
@@ -4,34 +4,32 @@ import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom'
 import { Button } from '@app/components/ui/buttons'
 import { ErrorDisplay } from '@app/components/ui/error-boundaries'
 import { Header } from '@app/components/ui/layouts'
+import { AppLoader } from '@app/components/ui/loaders'
 import { Routes } from '@app/router'
+import { Suspense } from 'react'
 
 export default function Error404Page() {
   const { t } = useTranslation('error-404-page')
   const error = useRouteError()
 
+  const renderErrorDisplay = () => {
+    if (!isRouteErrorResponse(error)) {
+      return <ErrorDisplay />
+    }
+
+    return error.status === 404 ? <ErrorDisplay is404Error /> : <ErrorDisplay error={error} />
+  }
+
   return (
     <>
       <Header />
-      <main className="min-h-[var(--container-height)] w-full px-8 pb-10 pt-20 sm:min-h-[var(--desktop-container-height)]">
+      <main className="grid min-h-[var(--container-height)] w-full px-8 pb-10 pt-20 sm:min-h-[var(--desktop-container-height)]">
         <div className="grid h-full w-full place-items-center overflow-x-hidden">
-          {(() => {
-            if (!isRouteErrorResponse(error)) {
-              return <ErrorDisplay />
-            }
-
-            return error.status === 404 ? (
-              <ErrorDisplay is404Error />
-            ) : (
-              <ErrorDisplay error={error} />
-            )
-          })()}
+          <Suspense fallback={<AppLoader />}>{renderErrorDisplay()}</Suspense>
 
           <div className="mt-32 text-center md:mt-40">
-            <Button size="lg">
-              <Link to={Routes.NETWORK.ROOT} className="text-inherit">
-                {t('backToHomePage')}
-              </Link>
+            <Button as={Link} size="lg" to={Routes.NETWORK.ROOT}>
+              {t('backToHomePage')}
             </Button>
           </div>
         </div>
