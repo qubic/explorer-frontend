3f103d650079ab7b39600a6950ce5faa6b5f6142
alexmf91 <alexmunoz91@gmail.com>
2024-08-19 23:22:14 +0200

    refactor: Add txType to TransactionWithStatus in convertHistoricalTxToTxWithStatus.ts

--- src/store/network/adapters/convertHistoricalTxToTxWithStatus.ts
+++ src/store/network/adapters/convertHistoricalTxToTxWithStatus.ts
@@ -1,4 +1,6 @@
 import type { HistoricalTx } from '@app/services/qli'
+import { TxTypeEnum } from '@app/types'
+import { isTransferTx } from '@app/utils/qubic-ts'
 import type { TransactionWithStatus } from '../txSlice'
 import convertHistoricalTxToLatestTx from './convertHistoricalTxToLatestTx'
 
@@ -7,6 +9,12 @@ export default function convertHistoricalTxToTxWithStatus(
 ): TransactionWithStatus {
   return {
     tx: convertHistoricalTxToLatestTx(historicalTx),
-    status: { txId: historicalTx.id, moneyFlew: historicalTx.moneyFlew }
+    status: {
+      txId: historicalTx.id,
+      moneyFlew: historicalTx.moneyFlew,
+      txType: isTransferTx(historicalTx.sourceId, historicalTx.destId, historicalTx.amount)
+        ? TxTypeEnum.TRANSFER
+        : TxTypeEnum.PROTOCOL
+    }
   }
 }
