e2b4f232ae3355342dc9c81ffd9bf7d208a23797
alexmf91 <alexmunoz91@gmail.com>
2024-08-20 21:37:42 +0200

    refactor: Update TxStatus component to conditionally render a transfer transaction icon

--- src/pages/network/components/TxStatus.tsx
+++ src/pages/network/components/TxStatus.tsx
@@ -2,19 +2,22 @@ import { CheckIcon, XmarkIcon } from '@app/assets/icons'
 
 type Props = {
   executed: boolean
+  isTransferTx: boolean
 }
 
-function TxStatus({ executed }: Props) {
-  return (
-    <div className="flex w-fit items-center gap-4 rounded-full bg-primary-60 px-8 py-4">
-      <p className="font-space text-xs text-gray-50">TX</p>
-      {executed ? (
-        <CheckIcon className="h-16 w-16 text-success-40" />
-      ) : (
-        <XmarkIcon className="h-16 w-16 text-error-40" />
-      )}
-    </div>
+function TxSatusIcon({ executed }: { executed: boolean }) {
+  return executed ? (
+    <CheckIcon className="h-16 w-16 text-success-40" />
+  ) : (
+    <XmarkIcon className="h-16 w-16 text-error-40" />
   )
 }
 
-export default TxStatus
+export default function TxStatus({ executed, isTransferTx }: Props) {
+  return (
+    <div className="flex w-fit items-center gap-4 rounded-full bg-primary-60 px-8 py-4">
+      <p className="font-space text-xs text-gray-50">TX</p>
+      {isTransferTx && <TxSatusIcon executed={executed} />}
+    </div>
+  )
+}
