02476790879959df32e9008b558246055ee21476
alexmf91 <alexmunoz91@gmail.com>
2024-08-23 00:59:42 +0200

    feat: Add richListReducer to networkReducer

--- src/store/network/networkReducer.ts
+++ src/store/network/networkReducer.ts
@@ -2,13 +2,15 @@ import { combineReducers } from '@reduxjs/toolkit'
 import addressReducer from './addressSlice'
 import blockReducer from './blockSlice'
 import overviewReducer from './overviewSlice'
+import richListReducer from './richListSlice'
 import txReducer from './txSlice'
 
 export const networkReducer = combineReducers({
   overview: overviewReducer,
   block: blockReducer,
   tx: txReducer,
-  address: addressReducer
+  address: addressReducer,
+  richList: richListReducer
 })
 
 export type NetworkState = ReturnType<typeof networkReducer>
