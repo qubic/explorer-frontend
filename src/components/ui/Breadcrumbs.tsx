8cc8dc1ded60b8a659924afc83ccab8aac7f93b0
alexmf91 <alexmunoz91@gmail.com>
2024-08-28 22:36:59 +0200

    refactor: Update Breadcrumbs component to use a smaller font size for the separator

--- src/components/ui/Breadcrumbs.tsx
+++ src/components/ui/Breadcrumbs.tsx
@@ -7,12 +7,12 @@ type Props = {
 export default function Breadcrumbs({ children }: Props) {
   return (
     <nav aria-label="Breadcrumbs" className="flex">
-      <ol className="flex items-center">
+      <ol className="flex items-center font-sans">
         {Children.map(children, (child, index) => (
           <Fragment key={`breadcrumb-${String(index)}`}>
             <li className="flex items-center">{child}</li>
             {index < Children.count(children) - 1 && (
-              <li aria-label="breadcrums-separator" className="mx-8 text-gray-500">
+              <li aria-label="breadcrums-separator" className="mx-8 text-sm text-gray-500">
                 /
               </li>
             )}
