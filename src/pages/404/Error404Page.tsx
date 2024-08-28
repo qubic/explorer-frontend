0dedb80dcbd5aa557c630b624b47f1fd04845f1f
alexmf91 <alexmunoz91@gmail.com>
2024-08-28 20:54:58 +0200

    refactor: Update Error404Page component to use Suspense for lazy loading

--- src/pages/404/Error404Page.tsx
+++ src/pages/404/Error404Page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
 import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom'
 
@@ -6,9 +7,8 @@ import { ErrorDisplay } from '@app/components/ui/error-boundaries'
 import { Header } from '@app/components/ui/layouts'
 import { AppLoader } from '@app/components/ui/loaders'
 import { Routes } from '@app/router'
-import { Suspense } from 'react'
 
-export default function Error404Page() {
+function Error404Page() {
   const { t } = useTranslation('error-404-page')
   const error = useRouteError()
 
@@ -25,7 +25,7 @@ export default function Error404Page() {
       <Header />
       <main className="grid min-h-[var(--container-height)] w-full px-8 pb-10 pt-20 sm:min-h-[var(--desktop-container-height)]">
         <div className="grid h-full w-full place-items-center overflow-x-hidden">
-          <Suspense fallback={<AppLoader />}>{renderErrorDisplay()}</Suspense>
+          {renderErrorDisplay()}
 
           <div className="mt-32 text-center md:mt-40">
             <Button as={Link} size="lg" to={Routes.NETWORK.ROOT}>
@@ -35,5 +35,13 @@ export default function Error404Page() {
         </div>
       </main>
     </>
+  )
+}
+
+export default function Error404PageWithSuspense() {
+  return (
+    <Suspense fallback={<AppLoader />}>
+      <Error404Page />
+    </Suspense>
   )
 }
