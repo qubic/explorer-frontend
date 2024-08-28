9e3140d92b248f0bc9afac8651af6cf01dbc8fe5
alexmf91 <alexmunoz91@gmail.com>
2024-08-28 22:33:19 +0200

    feat: Add generic error img

--- src/assets/images/errors/index.ts
+++ src/assets/images/errors/index.ts
@@ -1,3 +1,2 @@
-// Remove this eslint disable rule once we have other exports
-// eslint-disable-next-line import/prefer-default-export
+export { default as GenericError } from './error.svg?react'
 export { default as Error404 } from './error404.svg?react'
