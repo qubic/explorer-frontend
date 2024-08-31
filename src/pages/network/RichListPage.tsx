de8ec9c452356ec2285f3b0d6765c323660479e1
alexmf91 <alexmunoz91@gmail.com>
2024-08-31 13:09:27 +0200

    refactor: Improve RichListPage table layout and responsiveness

--- src/pages/network/RichListPage.tsx
+++ src/pages/network/RichListPage.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 
 import { Alert, Breadcrumbs, PaginationBar } from '@app/components/ui'
 import { LinearProgress } from '@app/components/ui/loaders'
+import { useTailwindBreakpoint } from '@app/hooks'
 import { getRichList, selectRichList } from '@app/store/network/richListSlice'
 import { formatString } from '@app/utils'
 import { useEffect, useMemo, useState } from 'react'
@@ -15,6 +16,7 @@ export default function RichListPage() {
   const dispatch = useAppDispatch()
   const { entities, paginationInfo, isLoading, error } = useAppSelector(selectRichList)
   const [page, setPage] = useState(1)
+  const { isMobile } = useTailwindBreakpoint()
 
   const handlePageChange = (value: number) => {
     setPage(value)
@@ -54,42 +56,50 @@ export default function RichListPage() {
             <p className="text-left text-sm text-gray-50">{t('richListWarning')}</p>
           </div>
           <div className="w-full rounded-12 border-1 border-primary-60 bg-primary-70">
-            <table className="w-full">
-              <thead className="border-b-1 border-primary-60 text-left font-space text-sm text-gray-50">
-                <tr>
-                  <th className="p-16 text-center font-400">
-                    <span className="hidden text-gray-50 sm:block">{t('rank')}</span>
-                  </th>
-                  <th className="p-16 font-400">
-                    <span className="text-gray-50">{t('addressID')}</span>
-                  </th>
-                  <th className="p-16 text-right font-400">
-                    <span className="text-gray-50">{t('amount')} (QUBIC)</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {error || entitiesWithRank?.length === 0 ? (
+            <div className="overflow-x-scroll">
+              <table className="w-full">
+                <thead className="border-b-1 border-primary-60 text-left font-space text-sm text-gray-50">
                   <tr>
-                    <td colSpan={3} className="p-32">
-                      <Alert variant="error">{t('richListLoadFailed')}</Alert>
-                    </td>
+                    <th className="p-16 text-center font-400">
+                      <span className="hidden text-gray-50 sm:block">{t('rank')}</span>
+                    </th>
+                    <th className="p-16 font-400">
+                      <span className="text-gray-50">{t('addressID')}</span>
+                    </th>
+                    <th className="p-16 text-right font-400">
+                      <span className="text-gray-50">{t('amount')} (QUBIC)</span>
+                    </th>
                   </tr>
-                ) : (
-                  entitiesWithRank?.map((entity) => (
-                    <tr key={entity.identity} className="border-b border-primary-60">
-                      <td className="p-16 text-center font-space text-sm">{entity.rank}</td>
-                      <td className="max-w-[30vw] overflow-hidden overflow-ellipsis whitespace-nowrap p-16 sm:max-w-[45vw] md:max-w-[50vw]">
-                        <AddressLink value={entity.identity} />
-                      </td>
-                      <td className="p-16 text-right font-space text-sm">
-                        {formatString(entity.balance)}
+                </thead>
+                <tbody>
+                  {error || entitiesWithRank?.length === 0 ? (
+                    <tr>
+                      <td colSpan={3} className="p-32">
+                        <Alert variant="error">{t('richListLoadFailed')}</Alert>
                       </td>
                     </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
+                  ) : (
+                    entitiesWithRank?.map((entity) => (
+                      <tr key={entity.identity} className="border-b border-primary-60">
+                        <td className="px-8 py-16 text-center font-space text-xs xs:text-sm sm:p-16">
+                          {entity.rank}
+                        </td>
+                        <td className="px-8 py-16 sm:p-16">
+                          <AddressLink
+                            value={entity.identity}
+                            ellipsis={isMobile}
+                            showTooltip={isMobile}
+                          />
+                        </td>
+                        <td className="px-8 py-16 text-right font-space text-xs xs:text-sm sm:p-16">
+                          {formatString(entity.balance)}
+                        </td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+            </div>
             <PaginationBar
               className="mx-auto w-fit gap-8 p-20"
               pageCount={paginationInfo.totalPages}
