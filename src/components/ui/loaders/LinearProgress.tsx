155452863d6033b2910b4fd92c0227f62134ed73
alexmf91 <alexmunoz91@gmail.com>
2024-08-31 21:27:58 +0200

    refactor: Update LinearProgress component to use a slightly lighter background color

--- src/components/ui/loaders/LinearProgress.tsx
+++ src/components/ui/loaders/LinearProgress.tsx
@@ -2,7 +2,7 @@ export default function LinearProgress() {
   return (
     <div className="w-full">
       <div className="h-1.5 w-full overflow-hidden bg-primary-60">
-        <div className="h-full w-full origin-left-right animate-progress bg-primary-50/30" />
+        <div className="h-full w-full origin-left-right animate-progress bg-primary-40/30" />
       </div>
     </div>
   )
