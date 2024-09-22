b1e58ed8f2ae432b9197b7dd59b5dc746e202eb3
alexmf91 <alexmunoz91@gmail.com>
2024-09-22 21:23:35 +0200

    feat: Add convertTxV2ToTxWithStatus adapter function

--- src/store/network/adapters/index.ts
+++ src/store/network/adapters/index.ts
@@ -1,2 +1,3 @@
 export { default as convertHistoricalTxToLatestTx } from './convertHistoricalTxToLatestTx'
 export { default as convertHistoricalTxToTxWithStatus } from './convertHistoricalTxToTxWithStatus'
+export { default as convertTxV2ToTxWithStatus } from './convertTxV2ToTxWithStatus'
