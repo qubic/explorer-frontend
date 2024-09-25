341237f44eaa98dde6b2d1ecb272a02c83510811
alexmf91 <alexmunoz91@gmail.com>
2024-09-25 22:47:54 +0200

    feat: Define __APP_VERSION__ in Vite config and update type declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,3 +10,6 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
+declare const __APP_VERSION__: string
