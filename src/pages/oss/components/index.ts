baf9ea296774394e844bd5a1430cce7e19c80f4b
Tomas Pozo <tomaspozogarzon@gmail.com>
2024-10-06 23:00:39 -0500

    Remove unused components

--- src/pages/oss/components/index.ts
+++ src/pages/oss/components/index.ts
@@ -1,10 +1,5 @@
-export { default as AddressLink } from './AddressLink'
 export { default as CardItem } from './CardItem'
 export { default as HomeLink } from './HomeLink'
 export { default as OverviewCardItem } from './OverviewCardItem'
 export { default as SubCardItem } from './SubCardItem'
-export { default as TickLink } from './TickLink'
-export { default as TickStatus } from './TickStatus'
-export { default as TxItem } from './TxItem/TxItem'
-export { default as TxLink } from './TxLink'
-export { default as TxStatus } from './TxStatus'
+
