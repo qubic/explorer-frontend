d6f801cf0aecd7cfba1ab3819481ab26f8c98b8a
Tomas Pozo <tomaspozogarzon@gmail.com>
2024-10-08 08:58:19 -0500

    Update qubic history endpoint

--- src/services/metrics/endpoints.ts
+++ src/services/metrics/endpoints.ts
@@ -6,7 +6,7 @@ export const METRICS_API_ENDPOINTS = {
   GITHUB_STATS: `${BASE_URL}/stats/github`,
   GITHUB_STATS_HISTORY: `${BASE_URL}/stats/github/history`,
   GITHUB_STATS_OVERVIEW: `${BASE_URL}/stats/github/overview`,
-  QUBIC_STATS: `${BASE_URL}/stats/qubic`
+  QUBIC_STATS: `${BASE_URL}/stats/qubic/history`
 } as const
 
 export default METRICS_API_ENDPOINTS
