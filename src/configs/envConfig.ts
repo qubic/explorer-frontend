1eb9face152dbe9a159c32b06d57d5adbe31a9e3
Tomas Pozo <tomaspozogarzon@gmail.com>
2024-11-19 11:53:40 -0500

    fix(analytics): fix metrics api token env name

--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -3,13 +3,15 @@ type EnvConfig = {
   ARCHIVER_API_URL: string
   QUBIC_API_URL: string
   METRICS_API_URL: string
+  METRICS_API_TOKEN: string
 }
 
 export const envConfig: EnvConfig = {
   QLI_API_URL: import.meta.env.VITE_QLI_API_URL,
   ARCHIVER_API_URL: import.meta.env.VITE_ARCHIVER_API_URL,
   QUBIC_API_URL: import.meta.env.VITE_QUBIC_API_URL,
-  METRICS_API_URL: import.meta.env.VITE_METRICS_API_URL
+  METRICS_API_URL: import.meta.env.VITE_METRICS_API_URL,
+  METRICS_API_TOKEN: import.meta.env.VITE_METRICS_API_TOKEN
 }
 
 export default envConfig
