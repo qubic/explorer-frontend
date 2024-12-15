801c8868825631ab4a3afb00ba36ea7d8e4d1295
alexmf91 <alexmunoz91@gmail.com>
2024-12-15 18:54:44 +0100

    refactor: remove overview slice and update network reducer

--- src/store/network/networkReducer.ts
+++ src/store/network/networkReducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import addressReducer from './addressSlice'
-import overviewReducer from './overviewSlice'
 import txReducer from './txSlice'
 
 export const networkReducer = combineReducers({
-  overview: overviewReducer,
   tx: txReducer,
   address: addressReducer
 })
