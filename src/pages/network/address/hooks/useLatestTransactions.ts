49a4505323e793ab9f57bd565b006fd3f924670a
alexmf91 <alexmunoz91@gmail.com>
2025-03-11 21:12:28 +0100

    feat: implement pagination for identity transfers in useLatestTransactions hook

--- src/pages/network/address/hooks/useLatestTransactions.ts
+++ src/pages/network/address/hooks/useLatestTransactions.ts
@@ -4,8 +4,9 @@ import type { GetAddressBalancesResponse } from '@app/store/apis/archiver-v1'
 import type { Transaction } from '@app/store/apis/archiver-v2'
 import { useLazyGetIndentityTransfersQuery } from '@app/store/apis/archiver-v2'
 
-const BATCH_SIZE = 50
-const TICK_SIZE = 200_000
+const PAGE_SIZE = 50
+const START_TICK = 1
+const END_TICK = 999_999_999 // Endpoint has now pagination but we still have to provide an end tick, we set it to a very large number
 
 export interface UseLatestTransactionsResult {
   transactions: Transaction[]
@@ -19,75 +20,39 @@ export default function useLatestTransactions(
   addressId: string,
   addressEndTick: GetAddressBalancesResponse['balance']['validForTick']
 ): UseLatestTransactionsResult {
-  const [startTick, setStartTick] = useState(Math.max(0, addressEndTick - TICK_SIZE))
-  const [transactions, setTransactions] = useState<Transaction[]>([])
   const [txsList, setTxsList] = useState<Transaction[]>([])
   const [isLoading, setIsLoading] = useState(false)
+  const [nextPage, setNextPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
   const cancellationRef = useRef(false)
 
   const [getIdentityTransfersQuery, { isFetching, error }] = useLazyGetIndentityTransfersQuery({})
 
-  const hasMore = startTick > 0
+  const fetchTransfers = useCallback(async () => {
+    const result = await getIdentityTransfersQuery({
+      addressId,
+      startTick: START_TICK,
+      endTick: END_TICK,
+      page: nextPage,
+      pageSize: PAGE_SIZE
+    }).unwrap()
 
-  const fetchTransfers = useCallback(
-    async (start: number, end: number) => {
-      const result = await getIdentityTransfersQuery({
-        addressId,
-        startTick: start,
-        endTick: end
-      }).unwrap()
-
-      return result || []
-    },
-    [getIdentityTransfersQuery, addressId]
-  )
-
-  const fetchRecursive = useCallback(
-    async (start: number, end: number, accumulatedData: Transaction[] = []) => {
-      if (cancellationRef.current) return { newTxs: accumulatedData, lastStartTick: start }
-
-      const newTxs = await fetchTransfers(start, end)
-      const combinedData = [...new Set(accumulatedData.concat(newTxs))]
-
-      if (combinedData.length < BATCH_SIZE && start > 0) {
-        const newEndTick = Math.max(0, start - 1)
-        const newStartTick = Math.max(0, start - 1 - TICK_SIZE)
-        return fetchRecursive(newStartTick, newEndTick, combinedData)
-      }
-
-      return {
-        newTxs: combinedData.sort((a, b) => b.transaction.tickNumber - a.transaction.tickNumber),
-        lastStartTick: start
-      }
-    },
-    [fetchTransfers]
-  )
+    return result
+  }, [getIdentityTransfersQuery, addressId, nextPage])
 
   const loadMoreTransactions = useCallback(async () => {
     if (isLoading || isFetching || !hasMore) return
 
     setIsLoading(true)
     try {
-      if (txsList.length < BATCH_SIZE) {
-        const newStartTick = Math.max(0, startTick - 1 - TICK_SIZE)
-        const newEndTick = Math.max(0, startTick - 1)
-        const { newTxs, lastStartTick } = await fetchRecursive(newStartTick, newEndTick)
-        // Since there could be some txs in txsList already, we need to merge them and then slice it
-        const updatedTxList = [...txsList, ...newTxs]
-        // Adding the new transactions to the list to be displayed
-        setTransactions((prev) => [...prev, ...updatedTxList.slice(0, BATCH_SIZE)])
-        // Updating the list of remaining transactions
-        setTxsList(updatedTxList.slice(BATCH_SIZE, updatedTxList.length))
-        // Updating the start and end tick
-        setStartTick(lastStartTick)
-      } else {
-        setTransactions((prev) => [...prev, ...txsList.slice(0, BATCH_SIZE)])
-        setTxsList((prevTxsList) => prevTxsList.slice(BATCH_SIZE, prevTxsList.length))
-      }
+      const { pagination, transactions } = await fetchTransfers()
+      setNextPage(pagination.nextPage)
+      setTxsList((prev) => [...prev, ...transactions])
+      setHasMore(pagination.nextPage !== -1)
     } finally {
       setIsLoading(false)
     }
-  }, [startTick, fetchRecursive, isLoading, isFetching, hasMore, txsList])
+  }, [isLoading, isFetching, hasMore, fetchTransfers])
 
   useEffect(() => {
     let isMounted = true
@@ -95,18 +60,17 @@ export default function useLatestTransactions(
 
     const initialFetch = async () => {
       setIsLoading(true)
-      const initialStartTick = Math.max(0, addressEndTick - TICK_SIZE)
-      const { newTxs, lastStartTick } = await fetchRecursive(initialStartTick, addressEndTick)
+      const { pagination, transactions } = await fetchTransfers()
 
       if (isMounted) {
-        setTransactions(newTxs.slice(0, BATCH_SIZE))
-        setTxsList(newTxs.slice(BATCH_SIZE, newTxs.length))
-        setStartTick(lastStartTick)
+        setNextPage(pagination.nextPage)
+        setTxsList((prev) => [...prev, ...transactions])
+        setHasMore(pagination.nextPage !== -1)
         setIsLoading(false)
       }
     }
 
-    if (transactions.length === 0 && addressEndTick) {
+    if (txsList.length === 0) {
       initialFetch()
     }
 
@@ -114,20 +78,20 @@ export default function useLatestTransactions(
       isMounted = false
       cancellationRef.current = true
     }
-  }, [fetchRecursive, transactions.length, addressEndTick])
+  }, [addressEndTick, fetchTransfers, txsList.length])
 
   useEffect(() => {
     return () => {
       if (addressId) {
-        setTransactions([])
         setTxsList([])
-        setStartTick(0)
+        setHasMore(true)
+        setNextPage(1)
       }
     }
   }, [addressId])
 
   return {
-    transactions,
+    transactions: txsList,
     loadMoreTransactions,
     hasMore,
     isLoading,
