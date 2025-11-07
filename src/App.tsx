b1e62f1d34a281f3a7a513071df60332d8a9b4dd
sallymoc <sallymoc.qubic@gmail.com>
2025-11-07 02:45:37 +0000

    chore: remove unused Turnstile functionality (#346)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { HelmetProvider } from 'react-helmet-async'
 import { I18nextProvider } from 'react-i18next'
 import { Provider as ReduxProvider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
-import { TurnstileProvider } from './components/providers'
 import i18n from './configs/i18n'
 import { router } from './router'
 import { store } from './store'
@@ -12,9 +11,7 @@ export default function App() {
     <HelmetProvider>
       <I18nextProvider i18n={i18n}>
         <ReduxProvider store={store}>
-          <TurnstileProvider>
-            <RouterProvider router={router} />
-          </TurnstileProvider>
+          <RouterProvider router={router} />
         </ReduxProvider>
       </I18nextProvider>
     </HelmetProvider>
