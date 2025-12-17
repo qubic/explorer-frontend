47e6bac4c85ba4971074893e6402dbdfc5ed2aa3
alez04 <alez2106x@gmail.com>
2025-12-17 04:19:45 +0200

    chore: improve skeleton accessibility

--- src/pages/network/assets/tokens/components/TokenSkeletonRow.tsx
+++ src/pages/network/assets/tokens/components/TokenSkeletonRow.tsx
@@ -1,12 +1,15 @@
 export default function TokenSkeletonRow() {
   return (
-    <tr className="animate-pulse border-b border-primary-60">
-      <td className="px-8 py-16 sm:p-16">
-        <div className="h-16 rounded bg-gray-70 xs:h-20" />
+    <tr className="animate-pulse border-b border-primary-60" aria-hidden="true">
+      <td className="px-8 py-16 sm:p-16" aria-label="Loading">
+        <div className="h-16 rounded bg-gray-70 xs:h-20" aria-hidden="true" />
       </td>
 
-      <td className="px-8 py-16 sm:p-16">
-        <div className="h-16 w-96 rounded bg-gray-70 xs:h-20 sm:h-40 sm:w-full sm:min-w-[248px] sm:max-w-[532px] 827px:h-20" />
+      <td className="px-8 py-16 sm:p-16" aria-label="Loading">
+        <div
+          className="h-16 w-96 rounded bg-gray-70 xs:h-20 sm:h-40 sm:w-full sm:min-w-[248px] sm:max-w-[532px] 827px:h-20"
+          aria-hidden="true"
+        />
       </td>
     </tr>
   )
--- src/pages/network/wallets/exchanges/components/ExchangeSkeletonRow.tsx
+++ src/pages/network/wallets/exchanges/components/ExchangeSkeletonRow.tsx
@@ -1,16 +1,19 @@
 export default function ExchangesSkeletonRow() {
   return (
-    <tr className="animate-pulse border-b border-primary-60">
-      <td className="px-8 py-16 sm:p-16">
-        <div className="h-16 w-full rounded bg-gray-70 xs:h-20" />
+    <tr className="animate-pulse border-b border-primary-60" aria-hidden="true">
+      <td className="px-8 py-16 sm:p-16" aria-label="Loading">
+        <div className="h-16 w-full rounded bg-gray-70 xs:h-20" aria-hidden="true" />
       </td>
 
-      <td className="px-8 py-16 sm:p-16">
-        <div className="h-16 w-96 rounded bg-gray-70 xs:h-20 sm:h-40 sm:w-full sm:min-w-[248px] sm:max-w-[532px] 827px:h-20" />
+      <td className="px-8 py-16 sm:p-16" aria-label="Loading">
+        <div
+          className="h-16 w-96 rounded bg-gray-70 xs:h-20 sm:h-40 sm:w-full sm:min-w-[248px] sm:max-w-[532px] 827px:h-20"
+          aria-hidden="true"
+        />
       </td>
 
-      <td className="px-8 py-16 text-right sm:p-16">
-        <div className="ml-auto h-16 w-136 rounded bg-gray-70 xs:h-20" />
+      <td className="px-8 py-16 text-right sm:p-16" aria-label="Loading">
+        <div className="ml-auto h-16 w-136 rounded bg-gray-70 xs:h-20" aria-hidden="true" />
       </td>
     </tr>
   )
