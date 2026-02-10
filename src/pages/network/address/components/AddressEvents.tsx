54c187376a710ac6b5661da599b187eab8f480b6
sallymoc <sallymoc.qubic@gmail.com>
2026-02-10 21:06:23 +0100

    feat: merge remove-historical-tab and sync address tabs URL

--- src/pages/network/address/components/AddressEvents.tsx
+++ src/pages/network/address/components/AddressEvents.tsx
@@ -1,4 +1,3 @@
-import { Skeleton } from '@app/components/ui'
 import TransactionEvents from '../../components/TxItem/TransactionEvents'
 import { useAddressEvents } from '../hooks'
 
@@ -7,19 +6,10 @@ type Props = Readonly<{ addressId: string }>
 export default function AddressEvents({ addressId }: Props) {
   const { events, isLoading } = useAddressEvents(addressId)
 
-  if (isLoading) {
-    return (
-      <div className="grid gap-10">
-        {Array.from({ length: 5 }).map((_, i) => (
-          <Skeleton key={String(`skeleton-${i}`)} className="h-48" />
-        ))}
-      </div>
-    )
-  }
-
   return (
     <TransactionEvents
       events={events}
+      isLoading={isLoading}
       paginated
       highlightAddress={addressId}
       showTickAndTimestamp
--- src/pages/network/tick/components/TickEvents.tsx
+++ src/pages/network/tick/components/TickEvents.tsx
@@ -1,4 +1,3 @@
-import { Skeleton } from '@app/components/ui'
 import TransactionEvents from '../../components/TxItem/TransactionEvents'
 import { useTickEvents } from '../hooks'
 
@@ -9,15 +8,5 @@ type Props = Readonly<{
 export default function TickEvents({ tick }: Props) {
   const { events, isLoading } = useTickEvents(tick)
 
-  if (isLoading) {
-    return (
-      <div className="grid gap-10">
-        {Array.from({ length: 5 }).map((_, i) => (
-          <Skeleton key={String(`skeleton-${i}`)} className="h-48" />
-        ))}
-      </div>
-    )
-  }
-
-  return <TransactionEvents events={events} paginated />
+  return <TransactionEvents events={events} isLoading={isLoading} paginated />
 }
